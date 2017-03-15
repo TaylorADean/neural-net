f744edbe9cde8bf4a14743fd208f32441b8b4248
Taylor Dean <taylor.dean@york.lan>
2017-03-15 00:22:35 -0400

    Initial Commit - Sample projects, etc.

---
+++ NeuralNetPlayground/NeuralNetCommon/ActivationMethods.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NeuralNetCommon
+{
+	public static class ActivationMethods
+	{
+		public static double ComputeLinear(double x)
+		{
+			return x;
+		}
+
+		public static double ComputeSigmoid(double x)
+		{
+			return 1.0 / (1.0 - Math.Exp(-1 * x));
+		}
+
+		public static double ComputeHyperbolicTangent(double x)
+		{
+			return (1 - Math.Exp(-2 * x)) / (1 + Math.Exp(2 * x));
+		}
+	}
+}
---
+++ NeuralNetPlayground/XOrNetworkTrainer/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NeuralNetFramework;
+using Networks.Example.XOr;
+
+namespace XOrNetworkTrainer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World");
+
+			var network = new Network();
+
+			// Assign training data:
+			network.Input1.Sum = 1;
+			network.Input2.Sum = 1;
+
+			ForwardPropagate(network);
+
+			Console.ReadLine();
+		}
+
+		private static void ForwardPropagate(Network network)
+		{
+			var inputNodes = new List<Node> { network.Input1, network.Input2 };
+
+			// Reset all values:
+			network.Output.Sum = 0;
+			foreach(var node in network.HiddenLayer)
+			{
+				node.Sum = 0;
+			}
+
+			// Forward propagate from input nodes to hidden layer.
+			foreach (var synapse in network
+				.Synapses
+				.Where(s => inputNodes.Any(n => n.Equals(s.InputNode))))
+			{
+				synapse.OutputNode.Sum += synapse.InputNode.Result * synapse.Weight;
+			}
+
+			// Forward propagate fromo hidden layer to output.
+			foreach(var synapse in network
+				.Synapses
+				.Where(s => network.HiddenLayer.Any(n => n.Equals(s.InputNode))))
+			{
+				synapse.OutputNode.Sum += synapse.InputNode.Result * synapse.Weight;
+			}
+
+			Console.WriteLine("Output Node Value: {0}", network.Output.Result);
+		}
+
+		private void BackwardPropogate(Network network, double calculatedResult)
+		{
+			const double target = 0;
+
+			var outputMarginOfError = target - calculatedResult;
+
+			// ******* Hack - using linear activation methods so d/dx == 1
+			var deltaOutputSum = 1.0 * outputMarginOfError;
+
+			var deltaWeights = 
+		}
+	}
+}
